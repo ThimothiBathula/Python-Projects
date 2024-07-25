01d46f6368821a63afbbac32c8841c639e24a3de
TIMOTHI BATHULA <bt3811783@gmail.com>
2024-07-25 19:31:35 +0530

    final commit

--- Quiz_App.py
+++ Quiz_App.py
@@ -1,8 +1,18 @@
 quizes={
-    "Which is the most sensitive organ in our body?":"skin"
+    "Q.1 Which is the “most sensitive organ” in our body?":"skin",
+    "Q.2 Who wrote “Romeo and Juliet”?":"william shakespeare",
+    "Q.3 Who painted the “Mona Lisa”?":"leonardo da vinci",
+    "Q.4 What is the smallest “prime number”?":"2",
+    "Q.5 What is the “currency of India”?":"rupee",
+    "Q.6 What is the “largest desert in the world”?":"antarctica",
+    "Q.7 Who was the “first man to step on the moon”?":"neil armstrong",
+    "Q.8 What is the “largest bird in the world”?":"ostrich",
+    "Q.9 Who is known as the “Father of Computers”?":"charles babbage",
+    "Q.10 In which year did “World War II end”?":"1945"
+
     }
 
-
+ 
 def Quiz():
     score=0
     for i in quizes:
@@ -13,8 +23,9 @@ def Quiz():
             print("Correct !")
             print("Your score:",score);
         else:
-            print("Incorrect !");
+            print("Incorrect ! correct Answer is ",quizes[i]);
             print("Your score:",score);
+    print("congrats ! your Score is:",score);
 opinion=input("Do you want to play Quize ('enter yes or no'):")
 if(opinion.lower()=='yes'):
     print("let's play");
